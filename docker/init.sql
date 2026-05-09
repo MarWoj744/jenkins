f9c3040f18278f4c7de83a48ec675f8b1b4e4c98
MarWoj744 <72458177+MarWoj744@users.noreply.github.com>
2026-05-10 00:34:18 +0200

    Delete docker directory

--- docker/init.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS product (
-                                       id BIGINT NOT NULL AUTO_INCREMENT,
-                                       name VARCHAR(255) NOT NULL,
-                                       PRIMARY KEY (id)
-);
-
-INSERT INTO product (name) VALUES ('product 1');
-INSERT INTO product (name) VALUES ('product 2');
