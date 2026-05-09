3298d1490acab5a911dc96777f7ebe2c26a29feb
MarWoj744 <72458177+MarWoj744@users.noreply.github.com>
2026-05-09 23:00:24 +0200

    Delete src directory

--- src/main/java/pl/example/crudapp/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package pl.example.crudapp.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import pl.example.crudapp.model.Product;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
